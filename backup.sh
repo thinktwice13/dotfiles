41652c5517664d3a253dfe00a64d490333cc6752
thinktwice13 <marioo.krajacic@gmail.com>
2021-01-08 17:03:10 +0100

    Update: 08-01-2021 @ 17:03:10

--- backup.sh
+++ backup.sh
@@ -8,14 +8,14 @@ timestamp() {
 # Files
 cp -pfv ~/.zshrc .
 cp -pfv ~/.gitconfig .
-# cp ~/.vimrc ~/dotfiles/.vimrc
+cp -pfv ~/.vimrc ~/dotfiles/
 cp -pRv ~/Library/ApplicationSupport/Code/User/keybindings.json ./Library/ApplicationSupport/Code/User/keybindings.json
 cp -pRv ~/Library/ApplicationSupport/Code/User/settings.json ./Library/ApplicationSupport/Code/User/settings.json
 
 # pull & push
 if [[ `git status --porcelain` ]]; then
-    git pull origin main
+    git pull origin master
     git add .
     git commit -m "Update: $(timestamp)"
-    git push origin main
+    git push origin master
 fi
